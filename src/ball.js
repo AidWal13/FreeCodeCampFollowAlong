9d6f8aa82c03f9d15ef601065d46027a49e7c58a
Aidan <aidanwalsh13@gmail.com>
2021-08-24 16:01:54 -0400

    add collision and bricks but no collision on bricks

--- src/ball.js
+++ src/ball.js
@@ -1,21 +1,56 @@
 export default class Ball {
-    constructor(gameWidth, gameHeight) {
+    constructor(game) {
         this.image = document.getElementById('img_ball')
 
-        this.gameWidth = gameWidth;
-        this.gameHeight = gameHeight;
+        this.gameWidth = game.gameWidth;
+        this.gameHeight = game.gameHeight;
+
+        this.game = game;
+
+        this.gameWidth = game.gameWidth;
+        this.gameHeight = game.gameHeight;
 
         this.position = {x: 10, y: 10}
-        this.speed = {x: 2, y: 2}
+        this.speed = {x: 4, y: 2}
         this.size = 16;
     }
 
     draw(ctx) {
-        ctx.drawImage(this.image, this.position.x, this.position.y, this.size, this.size);
+        ctx.drawImage(
+            this.image, 
+            this.position.x, 
+            this.position.y, 
+            this.size, 
+            this.size);
     }
 
     update(deltaTime) {
+        console.log(this.game.paddle.position.x)
         this.position.x += this.speed.x;
         this.position.y += this.speed.y;
+
+        //CHECK HIT DETECTION TO THE LEFT OR RIGHT
+        if(this.position.x + this.size > this.gameWidth || this.position.x < 0) {
+            this.speed.x = -this.speed.x;
+        }
+
+        //CHECK HIT DETECTION TO THE TOP OR BOTTOM
+        if(this.position.y + this.size > this.gameHeight || this.position.y < 0) {
+            this.speed.y = -this.speed.y;
+        }
+
+        //CHECK HIT DETECTION WITH PADDLE
+        let bottomOfBall = this.position.y + this.size;
+        let topOfPaddle = this.game.paddle.position.y;
+        let leftSideOfPaddle = this.game.paddle.position.x;
+        let rightSideOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
+
+        if (bottomOfBall >= topOfPaddle
+            && this.position.x >= leftSideOfPaddle
+            && this.position.x + this.size <= rightSideOfPaddle
+            ) {
+            this.speed.y = -this.speed.y;
+            this.position.y = this.game.paddle.position.y - this.size;
+        }
     }
 }
---
+++ src/game.js
@@ -0,0 +1,36 @@
+import Paddle from './paddle.js';
+import InputHandler from './input.js'
+import Ball from "./ball.js";
+import Brick from './brick.js';
+
+
+export default class Game {
+    constructor(gameWidth, gameHeight) {
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+    }
+
+    start() {
+        this.ball = new Ball(this);
+        this.paddle = new Paddle(this);
+
+        //let brick = new Brick(this, {x: 20, y: 40});
+        let bricks = [];
+
+        for(let i = 0; i<10; i++) {
+            bricks.push(new Brick(this, {x: i * 52, y: 30}))
+        }
+
+        this.gameObjects = [this.ball, this.paddle, ...bricks]
+
+        new InputHandler(this.paddle);
+    }
+
+    update(deltaTime) {
+        this.gameObjects.forEach((object) => object.update(deltaTime));
+    }
+
+    draw(ctx) {
+        this.gameObjects.forEach((object => object.draw(ctx)))
+    }
+}
